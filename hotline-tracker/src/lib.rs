8587742800624b109c67d98f39f7525b034fa840
Spike Grobstein <me@spike.cx>
2022-05-01 22:08:32 -0700

    adding support for registration records

--- hotline-tracker/src/lib.rs
+++ hotline-tracker/src/lib.rs
@@ -1,8 +1,10 @@
 mod update_record;
 mod server_record;
+mod registration_record;
 
 pub use update_record::UpdateRecord;
 pub use server_record::ServerRecord;
+pub use registration_record::RegistrationRecord;
 
 #[cfg(test)]
 mod tests {
---
+++ hotline-tracker/src/registration_record.rs
@@ -0,0 +1,77 @@
+use bytes::{Buf, BytesMut};
+use macroman_tools::macroman_to_string;
+
+const REGISTRY_VERSION: u16 = 1;
+
+/// the data that will be parsed from or encoded to a UDP packet
+/// for a hotline server tracker registration.
+///
+/// Because we should always receive this packet as a single packet,
+/// we only need to ensure that the packet is well-formed
+///
+/// TODO: this should return an error when parsing from bytes
+pub struct RegistrationRecord {
+    pub port: u16,
+    pub users_online: u16,
+    pub reserved: u16,
+    pub id: u32,
+    pub name: String,
+    pub description: String,
+    pub password: String,
+}
+
+impl RegistrationRecord {
+    pub fn from_bytes(bytes: &mut BytesMut) -> Option<Self> {
+        // these 15s are the static portion + length bytes (12 + 3)
+        if bytes.remaining() < 15 {
+            // not enough data for the absolute minimum size
+            return None
+        }
+
+        let ex_name_len = bytes[12] as usize;
+        if bytes.remaining() < 15 + ex_name_len {
+            return None
+        }
+
+        let ex_desc_len = bytes[12 + 1 + ex_name_len] as usize;
+        if bytes.remaining() < 15 + ex_name_len + ex_desc_len {
+            return None
+        }
+
+        let ex_pass_len = bytes[12 + 1 + ex_name_len + 1 + ex_desc_len] as usize;
+        if bytes.remaining() < 15 + ex_name_len + ex_desc_len + ex_pass_len {
+            return None
+        }
+
+        let version = bytes.get_u16();
+
+        assert_eq!(version, REGISTRY_VERSION);
+
+        let port = bytes.get_u16();
+        let users_online = bytes.get_u16();
+        let reserved = bytes.get_u16();
+        let id = bytes.get_u32();
+
+        let name_len = bytes.get_u8() as usize;
+        let name = macroman_to_string(&bytes[..name_len]);
+        bytes.advance(name_len);
+
+        let desc_len = bytes.get_u8() as usize;
+        let description = macroman_to_string(&bytes[..desc_len]);
+        bytes.advance(desc_len);
+
+        let pass_len = bytes.get_u8() as usize;
+        let password = macroman_to_string(&bytes[..pass_len]);
+        bytes.advance(pass_len);
+
+        Some(Self {
+            port,
+            users_online,
+            reserved,
+            id,
+            name,
+            description,
+            password,
+        })
+    }
+}
