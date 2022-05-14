abbd264fd9a2278eb092600d7b925cdaca5539d2
Spike Grobstein <me@spike.cx>
2022-05-14 10:46:43 -0700

    getting clap implemented for bookmark-cli

--- hotline-bookmark-cli/src/main.rs
+++ hotline-bookmark-cli/src/main.rs
@@ -1,4 +1,8 @@
+use std::process;
+
 use hotline_bookmark::bookmark::Bookmark;
+
+use clap::Parser;
 
 // usage:
 // hlbm print <bookmark>
@@ -7,22 +11,72 @@ use hotline_bookmark::bookmark::Bookmark;
 // use `-` for <address> if using json on STDIN (from output from tracker)
 // ~/.config/hlbm/hooks.json -- for hooks for connecting?
 
+#[derive(Parser, Debug)]
+struct PrintArgs {
+    file: String,
+
+    #[clap(short, long)]
+    json: bool,
+}
+
+#[derive(Parser, Debug)]
+struct CreateArgs {
+    file: String,
+    address: String,
+
+    #[clap(short, long)]
+    username: Option<String>,
+
+    #[clap(short, long)]
+    password: Option<String>,
+}
+
+#[derive(Parser, Debug)]
+enum Subcommand {
+    Print(PrintArgs),
+    Create(CreateArgs),
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(subcommand)]
+    subcommand: Subcommand,
+}
+
 fn main() {
-    let mut args: Vec<String> = std::env::args().collect();
-    args.remove(0);
+    let args = Args::parse();
 
-    for b in args {
-        println!("Opening {b}");
-        let bookmark = Bookmark::from_file(&b).unwrap();
-        println!("{:#?}", bookmark);
+    match args.subcommand {
+        Subcommand::Print(print_args) => {
+            match Bookmark::from_file(&print_args.file) {
+                Ok(bookmark) => {
+                    print_bookmark(&bookmark, &print_args);
+                },
+                Err(err) => {
+                    eprintln!("Error: {}", err);
+                    process::exit(1);
+                }
+            }
+        },
+        Subcommand::Create(create_args) => {
+            println!("creating...");
+            // let mut b2 = Bookmark::new("24.30.100.120".into());
+            // b2.credentials("star".into(), "guest".into());
+            // b2.write_to_file("./new_bookmark.hlbm").unwrap();
 
+            // let b = Bookmark::from_file("./new_bookmark.hlbm").unwrap();
+
+            // println!("{:?}", b);
+        },
     }
-    // let mut b2 = Bookmark::new("24.30.100.120".into());
-    // b2.credentials("star".into(), "guest".into());
-    // b2.write_to_file("./new_bookmark.hlbm").unwrap();
+}
 
-    // let b = Bookmark::from_file("./new_bookmark.hlbm").unwrap();
-
-    // println!("{:?}", b);
-
+fn print_bookmark(bookmark: &Bookmark, args: &PrintArgs) {
+    if args.json {
+        unimplemented!("no json yet");
+    } else {
+        println!("Address: {}", bookmark.address);
+        println!("Username: {}", bookmark.username);
+        println!("Password: {}", bookmark.password);
+    }
 }
