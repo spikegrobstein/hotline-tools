5e334f48e9b4c3d57169f62216e8f6750ffd7411
Spike Grobstein <me@spike.cx>
2022-05-14 11:03:09 -0700

    implemented more of the bookmark cli

--- hotline-bookmark-cli/src/main.rs
+++ hotline-bookmark-cli/src/main.rs
@@ -13,27 +13,37 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 struct PrintArgs {
+    /// The path to the bookmark file to operate on
     file: String,
 
+    /// Output the bookmark file as JSON for external parsing
     #[clap(short, long)]
     json: bool,
 }
 
 #[derive(Parser, Debug)]
 struct CreateArgs {
+    /// The path to the bookmark file we will create
     file: String,
+
+    /// The address of the server for the bookmark
     address: String,
 
-    #[clap(short, long)]
-    username: Option<String>,
+    /// Username to use to log in to the server
+    #[clap(short, long, default_value="")]
+    username: String,
 
-    #[clap(short, long)]
-    password: Option<String>,
+    /// Password to use to log in to the server
+    #[clap(short, long, default_value="")]
+    password: String,
 }
 
 #[derive(Parser, Debug)]
 enum Subcommand {
+    /// Print the contents of an existing bookmark file
     Print(PrintArgs),
+
+    /// Create a new bookmark file
     Create(CreateArgs),
 }
 
@@ -50,7 +60,7 @@ fn main() {
         Subcommand::Print(print_args) => {
             match Bookmark::from_file(&print_args.file) {
                 Ok(bookmark) => {
-                    print_bookmark(&bookmark, &print_args);
+                    print_bookmark(&bookmark, print_args);
                 },
                 Err(err) => {
                     eprintln!("Error: {}", err);
@@ -59,19 +69,18 @@ fn main() {
             }
         },
         Subcommand::Create(create_args) => {
-            println!("creating...");
-            // let mut b2 = Bookmark::new("24.30.100.120".into());
-            // b2.credentials("star".into(), "guest".into());
-            // b2.write_to_file("./new_bookmark.hlbm").unwrap();
-
-            // let b = Bookmark::from_file("./new_bookmark.hlbm").unwrap();
-
-            // println!("{:?}", b);
+            match create_bookmark(create_args) {
+                Err(err) => {
+                    eprintln!("Error: {}", err);
+                    process::exit(1);
+                },
+                _ => {},
+            }
         },
     }
 }
 
-fn print_bookmark(bookmark: &Bookmark, args: &PrintArgs) {
+fn print_bookmark(bookmark: &Bookmark, args: PrintArgs) {
     if args.json {
         unimplemented!("no json yet");
     } else {
@@ -79,4 +88,14 @@ fn print_bookmark(bookmark: &Bookmark, args: &PrintArgs) {
         println!("Username: {}", bookmark.username);
         println!("Password: {}", bookmark.password);
     }
+}
+
+fn create_bookmark(args: CreateArgs) -> Result<(), Box<dyn std::error::Error>> {
+    let mut b = Bookmark::new(args.address.clone());
+    b.credentials(args.username, args.password);
+    b.write_to_file(&args.file)?;
+
+    eprintln!("Wrote bookmark: {}", args.file);
+
+    Ok(())
 }
